90b633c4cce14c3f64fa1076b91e00272a8c659b
hkjin <wlsgusrb78@naver.com>
2025-02-09 21:42:02 +0900

    init webflux

---
+++ webflux-lab/src/main/java/org/hyeonqz/webfluxlab/WebfluxLabApplication.java
@@ -0,0 +1,13 @@
+package org.hyeonqz.webfluxlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxLabApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run(WebfluxLabApplication.class, args);
+	}
+
+}
---
+++ webflux-lab/src/test/java/org/hyeonqz/webfluxlab/WebfluxLabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyeonqz.webfluxlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxLabApplicationTests {
+
+	@Test
+	void contextLoads () {
+	}
+
+}
